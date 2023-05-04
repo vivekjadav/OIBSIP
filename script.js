23ada71aabb3c5d5f8f2ec872a4ab4a11454df78
vivekjadav <107034093+vivekjadav@users.noreply.github.com>
2023-05-04 19:25:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function myfunction(){
+    alert("sign in to continue shopping...!");
+  }
+  
+  // javascript to make side menu appear
+  
+  function openNav() {
+    document.getElementById("sidenav").style.width = "50%";
+  }
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0%";
+  }
